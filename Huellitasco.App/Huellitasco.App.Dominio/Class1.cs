c660afba102763b721c9dc066f7c1aace734c3d2
David Alejandro Tolosa <davito022196@gmail.com>
2022-10-07 08:49:29 -0500

    capa consola para pruebas

--- Huellitasco.App/Huellitasco.App.Dominio/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Huellitasco.App.Dominio
-{
-    public class Class1
-    {
-    }
-}
---
+++ Huellitasco.App/Huellitasco.App.Persistencia/AppRepositorios/AppContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Huellitasco.App.Dominio;
+namespace Huellitasco.App.Persistencia
+{
+    public class AppContext:DbContext
+    {
+        public DbSet<Persona> Personas {get; set;}
+        public DbSet<Veterinario> Veterinario {get; set;}
+        public DbSet<Dueno> Dueno {get; set;}
+        public DbSet<VisitasPyP> VisitasPyP {get; set;}
+        public DbSet<Historia> Historia {get; set;}
+        public DbSet<Mascota> Mascota {get; set;}
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = HuellistacoData");
+            }
+        }
+    }
+}
